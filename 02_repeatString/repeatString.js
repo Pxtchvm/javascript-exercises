de249702c1085744d1baa78de48c0d2582b2470d
Jacob Duldulao <111462541+Pxtchvm@users.noreply.github.com>
2025-02-03 19:21:15 +0800

    Solve 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
+    let returnString = '';
+    
+    if (times < 0) {
+        return 'ERROR';
+    }
 
+    for (let i = 0; i < times; i++) {
+        returnString += string;
+    }
+
+    return returnString;
 };
 
 // Do not edit below this line
