13a9f73c987e5fbd8ffcb75cfa2d178372021efa
Jacob Duldulao <111462541+Pxtchvm@users.noreply.github.com>
2025-02-04 15:12:48 +0800

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    let largerNumber;
+    let smallerNumber;
+    let sum = 0;
+    
+    if (!(typeof firstNumber === "number") || !(typeof secondNumber === "number")) {
+        return "ERROR";
+    }
 
+    if (firstNumber < 0 || secondNumber < 0) {
+        return "ERROR";
+    }
+    
+    if (!(Number.isInteger(firstNumber)) || !(Number.isInteger(secondNumber))) {
+        return "ERROR";
+    }
+
+    if (firstNumber > secondNumber) {
+        largerNumber = firstNumber;
+        smallerNumber = secondNumber;
+    } else {
+        largerNumber = secondNumber;
+        smallerNumber = firstNumber;
+    }
+
+    for (let i = smallerNumber; i <= largerNumber; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
