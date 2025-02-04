7e80989d5f77f8a0545a7866a1cf440e01e412b1
Jacob Duldulao <111462541+Pxtchvm@users.noreply.github.com>
2025-02-04 15:32:32 +0800

    Solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  const temperatureInCelsius = (temperature - 32) * (5 / 9);
+  return +(temperatureInCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  const temperatureInFahrenheit = temperature * (9 / 5) + 32;
+  return +(temperatureInFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
