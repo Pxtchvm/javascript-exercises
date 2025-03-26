dcc006148d7f185f9c05c5257979d07516ec6905
Jacob Duldulao <111462541+Pxtchvm@users.noreply.github.com>
2025-03-26 10:39:09 +0800

    Add solution to 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+  if (typeof number === "string") {
+    number = +number;
+  }
+  if (number < 0) {
+    return "OOPS";
+  }
+  if (number === 1) {
+    return 1;
+  }
+  let fibonacci = 1;
+  let previous = 0;
+  let sum = 0;
+  for (let i = 1; i < number; i++) {
+    sum = fibonacci + previous;
+    previous = fibonacci;
+    fibonacci = sum;
+  }
+  return sum;
 };
 
 // Do not edit below this line
