e9644566d30cd35089c1285c496b974234a6f619
Jacob Duldulao <111462541+Pxtchvm@users.noreply.github.com>
2025-03-26 09:54:04 +0800

    Add solution to 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,72 @@
-const palindromes = function () {
-
+/**
+ *
+ * @param {String} string
+ */
+const palindromes = function (string) {
+  let formattedString = removePunctuation(string.toLowerCase());
+  let middleIndex = Math.floor(formattedString.length / 2);
+  if (formattedString.length % 2 === 0) {
+    return (
+      formattedString.slice(0, middleIndex) ===
+      reverse(formattedString.slice(middleIndex, formattedString.length))
+    );
+  }
+  return (
+    formattedString.slice(0, middleIndex) ===
+    reverse(formattedString.slice(middleIndex + 1, formattedString.length))
+  );
 };
 
+function reverse(string) {
+  let reversedString = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedString += string.at(i);
+  }
+  return reversedString;
+}
+
+function removePunctuation(string) {
+  const punctuationArray = [
+    "!",
+    '"',
+    "#",
+    "$",
+    "%",
+    "&",
+    "'",
+    "(",
+    ")",
+    "*",
+    "+",
+    ",",
+    "-",
+    ".",
+    "/",
+    ":",
+    ";",
+    "?",
+    "@",
+    "[",
+    "\\",
+    "]",
+    "^",
+    "_",
+    "`",
+    "{",
+    "|",
+    "}",
+    "~",
+    " ",
+  ];
+  let newString = "";
+  for (let i = 0; i < string.length; i++) {
+    let currentChar = string.at(i);
+    if (punctuationArray.includes(currentChar)) {
+      continue;
+    }
+    newString += currentChar;
+  }
+  return newString;
+}
 // Do not edit below this line
 module.exports = palindromes;
