a1514aa461cd839a04111e34cd58dd0c34c13867
Jacob Duldulao <111462541+Pxtchvm@users.noreply.github.com>
2025-03-26 11:35:27 +0800

    Add solution for 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,44 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  return array.reduce(
+    (oldestPerson, currentPerson) => {
+      let currentPersonAge;
+      let oldestPersonAge;
+      if (currentPerson.yearOfDeath === undefined) {
+        currentPersonAge = new Date().getFullYear() - currentPerson.yearOfBirth;
+      } else {
+        currentPersonAge =
+          currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+      }
+      if (oldestPerson.yearOfDeath === undefined) {
+        oldestPersonAge = new Date().getFullYear() - oldestPerson.yearOfBirth;
+      } else {
+        oldestPersonAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+      }
+      if (currentPersonAge > oldestPersonAge) {
+        return currentPerson;
+      }
+      return oldestPerson;
+    });
 };
+
+// Better Solution:
+/* 
+const findTheOldest = function(array) {
+  return array.reduce((oldestPerson, currentPerson) => {
+    // Calculate current person's age
+    const currentPersonAge = currentPerson.yearOfDeath 
+      ? currentPerson.yearOfDeath - currentPerson.yearOfBirth 
+      : new Date().getFullYear() - currentPerson.yearOfBirth;
+    
+    // Calculate oldest person's age found so far
+    const oldestPersonAge = oldestPerson.yearOfDeath 
+      ? oldestPerson.yearOfDeath - oldestPerson.yearOfBirth 
+      : new Date().getFullYear() - oldestPerson.yearOfBirth;
+    
+    // Return the person with the higher age
+    return currentPersonAge > oldestPersonAge ? currentPerson : oldestPerson;
+  });
+}; */
 
 // Do not edit below this line
 module.exports = findTheOldest;
